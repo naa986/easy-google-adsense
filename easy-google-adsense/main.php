f94c3968af10e0c098e92fdd49149820704cca91
naa986 <noorbd28@gmail.com>
2020-08-10 12:32:07 +0600

    .first commit

---
+++ easy-google-adsense/main.php
@@ -0,0 +1,135 @@
+<?php
+/*
+Plugin Name: Easy Google AdSense
+Version: 1.0.3
+Plugin URI: https://noorsplugin.com/easy-google-adsense-plugin-wordpress/
+Author: naa986
+Author URI: https://noorsplugin.com/
+Description: Easily add Google AdSense to your WordPress site
+Text Domain: easy-google-adsense
+Domain Path: /languages 
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+if (!class_exists('EASY_GOOGLE_ADSENSE')) {
+
+    class EASY_GOOGLE_ADSENSE {
+
+        var $plugin_version = '1.0.3';
+
+        function __construct() {
+            define('EASY_GOOGLE_ADSENSE_VERSION', $this->plugin_version);
+            $this->plugin_includes();
+        }
+
+        function plugin_includes() {
+            if (is_admin()) {
+                add_filter('plugin_action_links', array($this, 'plugin_action_links'), 10, 2);
+            }
+            add_action('plugins_loaded', array($this, 'plugins_loaded_handler'));
+            add_action('admin_init', array($this, 'settings_api_init'));
+            add_action('admin_menu', array($this, 'add_options_menu'));
+            add_action('wp_head', array($this, 'add_adsense_auto_ads_code'));
+        }
+        
+        function plugins_loaded_handler()
+        {
+            load_plugin_textdomain('easy-google-adsense', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/'); 
+        }
+
+        function plugin_url() {
+            if ($this->plugin_url)
+                return $this->plugin_url;
+            return $this->plugin_url = plugins_url(basename(plugin_dir_path(__FILE__)), basename(__FILE__));
+        }
+
+        function plugin_action_links($links, $file) {
+            if ($file == plugin_basename(dirname(__FILE__) . '/main.php')) {
+                $links[] = '<a href="options-general.php?page=easy-google-adsense-settings">'.__('Settings', 'easy-google-adsense').'</a>';
+            }
+            return $links;
+        }
+        function add_options_menu() {
+            if (is_admin()) {
+                add_options_page(__('Easy Google AdSense', 'easy-google-adsense'), __('Easy Google AdSense', 'easy-google-adsense'), 'manage_options', 'easy-google-adsense-settings', array($this, 'options_page'));
+            }
+        }
+        function sanitize($input)
+        {
+            $sanitized_input = array();
+            if(isset($input['publisher_id'])){
+                $sanitized_input['publisher_id'] = sanitize_text_field($input['publisher_id']);
+            }
+            return $sanitized_input;
+        }
+        function settings_api_init(){
+            $args = array( 
+                'sanitize_callback' => array( $this, 'sanitize' ) // using a custom function to sanitize since the API doesn't allow array just yet
+            );
+            register_setting( 'easygoogleadsensepage', 'easy_google_adsense_settings', $args );
+            //register_setting( 'easygoogleadsensepage', 'easy_google_adsense_settings' );
+
+            add_settings_section(
+                    'easy_google_adsense_section', 
+                    __('General Settings', 'easy-google-adsense'), 
+                    array($this, 'easy_google_adsense_settings_section_callback'), 
+                    'easygoogleadsensepage'
+            );
+
+            add_settings_field( 
+                    'publisher_id', 
+                    __('Publisher ID', 'easy-google-adsense'), 
+                    array($this, 'publisher_id_render'), 
+                    'easygoogleadsensepage', 
+                    'easy_google_adsense_section' 
+            );
+        }
+        function publisher_id_render() { 
+            $options = get_option('easy_google_adsense_settings');            
+            ?>
+            <input type='text' name='easy_google_adsense_settings[publisher_id]' value='<?php echo $options['publisher_id']; ?>'>
+            <p class="description"><?php printf(__('Enter your Google AdSense Publisher ID (e.g %s).', 'easy-google-adsense'), 'pub-1234567890111213');?></p>
+            <?php
+        }
+        function easy_google_adsense_settings_section_callback() { 
+                //echo __( 'This section description', 'easygoogleadsense' );
+        }
+
+        function options_page() {
+            $url = "https://noorsplugin.com/easy-google-adsense-plugin-wordpress/";
+            $link_text = sprintf(wp_kses(__('Please visit the <a target="_blank" href="%s">Easy Google AdSense</a> documentation page for full setup instructions.', 'easy-google-adsense'), array('a' => array('href' => array(), 'target' => array()))), esc_url($url));
+            ?>           
+            <div class="wrap">               
+            <h2>Easy Google AdSense - v<?php echo $this->plugin_version; ?></h2>
+            <div class="update-nag"><?php echo $link_text;?></div>
+            <form action='options.php' method='post'>
+            <?php
+            settings_fields('easygoogleadsensepage');
+            do_settings_sections('easygoogleadsensepage');
+            submit_button();
+            ?>
+            </form>
+            </div>
+            <?php
+        }
+        
+        function add_adsense_auto_ads_code() {
+            $options = get_option('easy_google_adsense_settings');
+            $publisher_id = $options['publisher_id'];
+            if(isset($publisher_id) && !empty($publisher_id)){
+                $ouput = <<<EOT
+                <!-- auto ad code generated with Easy Google AdSense plugin v{$this->plugin_version} -->
+                <script data-ad-client="ca-{$publisher_id}" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>      
+                <!-- / Easy Google AdSense plugin -->
+EOT;
+
+                echo $ouput;
+            }
+        }
+
+    }
+
+    $GLOBALS['easy_google_adsense'] = new EASY_GOOGLE_ADSENSE();
+}
---
+++ easy-google-adsense/readme.txt
@@ -0,0 +1,86 @@
+=== Easy Google AdSense ===
+Contributors: naa986
+Donate link: https://noorsplugin.com/
+Tags: adsense, google, google adsense, ads, ad manager
+Requires at least: 4.9
+Tested up to: 5.5
+Requires PHP: 5.2.4
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily add Google AdSense ad code to your WordPress site. Automatically show Google ads optimized for your site at optimal times and increase revenue.
+
+== Description ==
+
+[Easy Google AdSense](https://noorsplugin.com/easy-google-adsense-plugin-wordpress/) plugin makes Google AdSense monetization easier for WordPress. With this plugin, there is no need to manually add AdSense ads/code to different parts of your website. You just enter your Google AdSense Publisher ID in the settings and Google will automatically start showing ads at optimal times when they are likely to perform well for your website and provide a good experience for your users.
+
+=== What is Google AdSense Auto Ads? === 
+
+Auto Ads is an improved monetization method in Google AdSense. It uses machine learning to make smart ad placement for your website. When this is enabled, Google will analyze your site and show ads only when it thinks they will perform well and provide a good user experience.
+
+=== Benefits of Google AdSense Auto Ads ===
+
+* Ads are automatically optimized using machine learning
+* Let Google handle ad placement and comply with terms and conditions easily
+* Increase revenue by showing ads when they will perform well
+* Automatically show Text & display ads, In-feed ads, In-article ads, Matched content, Anchor ads and Vignette ads at optimal times
+* Let Google understand the user and show ads at various places on your website 
+* There is no conflict since it takes into consideration any ad code that you have already placed on your website
+* It works with page-level ads since page-level ads have been migrated into auto ads
+
+=== How to Use Google AdSense Auto Ads ===
+
+https://www.youtube.com/watch?v=C39cZGX-mrk&rel=0
+
+1. Install and Activate the *Easy Google AdSense* plugin
+1. Login to Google AdSense account
+1. Go to **Settings > Account > Account information** and copy your **AdSense Publisher ID**
+1. Login to your WordPress admin dashboard and go to **Settings > Easy Google AdSense**.
+1. Paste your AdSense Publisher ID into the **Publisher ID** field
+1. Click **Save Changes**
+1. Go back to your Google AdSense account dashboard and navigate to **My ads > Content > Auto ads**.
+1. Select ad types that you wish to automatically show on your website
+1. Click **SAVE**
+
+For documentation please visit the Easy [Google AdSense](https://noorsplugin.com/easy-google-adsense-plugin-wordpress/) plugin page.
+
+== Installation ==
+
+1. Go to the Add New plugins screen in your WordPress Dashboard
+1. Click the upload tab
+1. Browse for the plugin file (easy-google-adsense.zip) on your computer
+1. Click "Install Now" and then hit the activate button
+
+== Frequently Asked Questions ==
+
+= Can this plugin be used to automatically add Google AdSense code to a WordPress site? =
+
+Yes.
+
+= Can I use this plugin if I already have some Google ads configured on my website? =
+
+Yes.
+
+== Screenshots ==
+
+1. Easy Google AdSense Settings
+2. Google AdSense Auto Ads Settings
+
+== Upgrade Notice ==
+
+none
+
+== Changelog ==
+
+= 1.0.3 =
+
+* Updated the ad code to use the latest AdSense API
+
+= 1.0.2 =
+
+* Easy Google AdSense now trims the value entered in the settings
+
+= 1.0.1 =
+
+* First commit
